8d1ddbbbeed7b2f55606d9ed7cf290e25733f909
Marek Strypling <C:\Users\marek\AppData\Roaming\The Bat!>
2017-05-26 10:07:30 +0200

    add new respository 'zadanie_8_5'

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Zadanie 8.5
+
+var a = prompt('Wpisz wartosc a'),
+	h = prompt('Wpisz wartosc h'),
+	triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+//console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+document.write('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
